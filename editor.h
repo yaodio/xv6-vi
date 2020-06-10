0b8e0552140f7a9421fc3223860ea45297a9567e
yaodio <2017151071@email.szu.edu.cn>
2020-06-10 17:14:54 +0800

    修改全局变量tx的定义位置

--- editor.h
+++ editor.h
@@ -1,19 +1,21 @@
+#define NULL          0       // 空指针
+
 // 行结构体（双向链表节点）
 typedef struct line {
-  char chs[SCREEN_WIDTH+1]; // 一行字符，+1是为了末尾填入'\0'
-  uint n;                   // 该行的字符数，理论上应该 n <= SCREEN_WIDTH
-  struct line *prev;        // 上一行指针
-  struct line *next;        // 下一行指针
-  int paragraph;            // 当为1时表示与下一行是同一段
+  char chs[SCREEN_WIDTH+1];   // 一行字符，+1是为了末尾填入'\0'
+  uint n;                     // 该行的字符数，理论上应该 n <= SCREEN_WIDTH
+  struct line *prev;          // 上一行指针
+  struct line *next;          // 下一行指针
+  int paragraph;              // 当为1时表示与下一行是同一段
 } line;
 
 // 文本结构体（双向链表，每个节点表示一行）
-typedef struct text {
-  char *path;   // 文件路径
-  line *head;   // 首行
-  line *tail;   // 尾行
-  line *show;   // 屏幕上显示的第0行
-} text;
+static struct text {
+  char *path;                 // 文件路径
+  line *head;                 // 首行
+  line *tail;                 // 尾行
+  line *show;                 // 屏幕上显示的第0行
+} tx = {NULL,NULL,NULL,NULL}; // 全局文档变量
 
 // 键盘按键
 #define KEY_UP        0xE2    // 上
@@ -21,8 +23,3 @@ typedef struct text {
 #define KEY_LF        0xE4    // 左
 #define KEY_RT        0xE5    // 右
 #define KEY_ESC       0x1B    // ESC
-
-#define NULL          0       // 空指针
-
-// 全局变量
-static text tx = {NULL, NULL, NULL};  // 文档
